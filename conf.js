b4619dc74fbae4f4845d49cc86853e9910b66e39
Abdurahman-Abdella <abdellahayan645@gmail.com>
2025-06-30 23:55:12 +0300

    Add keyboard controls and update button styles
    Added keyboard shortcuts for rock ('r'), paper ('p'), scissors ('s'), and score reset ('0') in conf.js. Updated .move-button and .auto-play-button styles in style.css for improved appearance and interactivity, and removed unused hover effect on .container.

--- conf.js
+++ conf.js
@@ -46,7 +46,24 @@
         autoPlay();
 });
 
-
+document.body.addEventListener('keydown', (event) => {
+   if(event.key === 'r'){
+    playGame('rock')
+   }else if(event.key === 'p'){
+    playGame('paper')
+   }
+   else if(event.key === 's'){
+    playGame('scissors')
+   }
+   else if(event.key === '0'){
+      score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement();
+   }
+   
+})
 
 
     function playGame(playerMove) {
